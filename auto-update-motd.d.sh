be38bf5a22ead2bc16294dd18f713303c485659c
qljsyph <245438367@qq.com>
2024-12-15 23:12:57 +0800

    修改bug

--- auto-update-motd.d.sh
+++ auto-update-motd.d.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
-# 检查 /etc/profile 是否已经包含完整的代码块
-grep -q 'if [ -n "$SSH_CONNECTION" ]; then' /etc/profile && \
-grep -q 'run-parts /etc/update-motd.d' /etc/profile && \
-grep -q 'fi' /etc/profile
+# 定义要检查的代码块
+check_code='if [ -n "$SSH_CONNECTION" ]; then
+    run-parts /etc/update-motd.d
+fi'
 
-if [ $? -ne 0 ]; then
+# 检查 /etc/profile 是否已经包含要添加的代码块
+if ! grep -qF -- "$check_code" /etc/profile; then
     # 将代码追加到 /etc/profile 的末尾
-    echo -e '\nif [ -n "$SSH_CONNECTION" ]; then\n    run-parts /etc/update-motd.d\nfi' >> /etc/profile
+    echo -e "$check_code" >> /etc/profile
     echo "内容已添加到 /etc/profile"
 else
     echo "所需内容已存在于 /etc/profile"
