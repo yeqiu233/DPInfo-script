1b94cae53391b1fa7b977b4362ce984898c0cdd2
qljsyph <245438367@qq.com>
2024-12-15 23:09:38 +0800

    增强检查精度避免重复添加

--- auto-update-motd.d.sh
+++ auto-update-motd.d.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
-# 检查 /etc/profile 是否已经包含所需的内容
-grep -q 'if [ -n "$SSH_CONNECTION" ]; then' /etc/profile
+# 检查 /etc/profile 是否已经包含完整的代码块
+grep -q 'if [ -n "$SSH_CONNECTION" ]; then' /etc/profile && \
+grep -q 'run-parts /etc/update-motd.d' /etc/profile && \
+grep -q 'fi' /etc/profile
+
 if [ $? -ne 0 ]; then
     # 将代码追加到 /etc/profile 的末尾
     echo -e '\nif [ -n "$SSH_CONNECTION" ]; then\n    run-parts /etc/update-motd.d\nfi' >> /etc/profile
